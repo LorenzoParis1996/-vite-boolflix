5c6410dc95a662cb1da438daf29aa4b07425d128
LorezoParis1996 <lorenzoparis86@live.it>
2024-06-14 11:34:55 +0200

    creato progetto

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data() {
+
+  },
+  components: {
+    AppHeader
+  }
+}
+
+</script>
+
+<template>
+ <AppHeader/>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss'
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+
+
+export default {
+  data() {
+     return {
+
+     }
+  }
+}
+
+</script>
+
+<template>
+ <h1>header</h1>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss'
+
+</style>
